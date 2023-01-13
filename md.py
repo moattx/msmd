6b9efa69d2ddf5b47f19fded532a1e65c41117e0
moatx <wowzers1lol@gmail.com>
2023-01-13 06:32:19 +0300

    fix: go to the directory where it was run from

--- md.py
+++ md.py
@@ -8,8 +8,9 @@ from yt_dlp import YoutubeDL
 class Client:
     def __init__(self):
         self.playlists = []
+        self.playlist = ""
         self.ytd_file_name = ""
-        self.current_dir = os.getcwd()
+        self.path = os.getcwd()
 
     def run(self):
         self.parse()
@@ -21,48 +22,56 @@ class Client:
 
             # hastag for playlist (directory)
             if self.playlists[number][0] == "#":
-                if os.getcwd() != self.current_dir:
-                    os.chdir(self.current_dir)
+                if self.playlist:
+                    print("done with " + self.playlist + " making new playlist...")
 
-                playlist_name = self.playlists[number].strip("#")
-                print("creating a playlist named " + playlist_name + "...")
-                os.mkdir(playlist_name)
-                os.chdir(playlist_name)
+                if os.getcwd() != self.path:
+                    os.chdir(self.path)
+
+                self.playlist = self.playlists[number].strip("#")
+                print("creating a playlist named " + self.playlist + "...")
+                os.mkdir(self.playlist)
+                os.chdir(self.playlist)
+                # skip playlist name
                 number = number + 1
                 continue
 
+            # skip link
+            number = number + 1
+            file_name = self.playlists[number] + ".mp4"
+
+            print("music = " + file_name + " for " + self.playlist)
+            # if len(self.ytd_file_name) != 0:
             ydl_opts = {
                 "format": "m4a/bestaudio/best",
+                "outtmpl": file_name,
                 "postprocessors": [
                     {
                         "key": "FFmpegExtractAudio",
                         "preferredcodec": "m4a",
                     }
                 ],
-                "progress_hooks": [self.my_hook],
             }
-            with YoutubeDL(ydl_opts) as ydl:
-                ydl.download([self.playlists[number]])
 
-            number = number + 1
-            file_name = self.playlists[number] + ".mp4"
+            print("downloading music...")
+            with YoutubeDL(ydl_opts) as ydl:
+                try:
+                    # INFO: decrement for the link
+                    ydl.download([self.playlists[(number - 1)]])
+                except:
+                    pass
 
             # update to go to next link or playlist name
             number = number + 1
 
-            print("FILE_NAME = " + self.ytd_file_name)
-            if len(self.ytd_file_name) != 0:
-                os.rename(self.ytd_file_name, file_name)
-        # finally change back to the previos directory
-        # TODO: check if this is neccesary
-        os.chdir("..")
+        print("goodbye...")
 
     def parse(self):
         # tmplist = []
         skip = False
         # TODO: find a better way to get home dir?
         with open(os.environ["HOME"] + "/.md.conf", "r", encoding="UTF-8") as file:
-            lines = list(line for line in (l.strip() for l in file) if line)
+            lines = list(line for line in (line.strip() for line in file) if line)
             for line in lines:
                 if line[0] == "#":
                     playlist = line.strip("#")
@@ -86,10 +95,6 @@ class Client:
                     self.playlists.append(line.split(" ")[0])
                     # name
                     self.playlists.append(line.split(" ")[1])
-
-    def my_hook(self, d):
-        if d["status"] == "finished":
-            self.ytd_file_name = d["filename"]
 
 
 # MAIN
